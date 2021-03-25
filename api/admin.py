274b2a3827ae19295a9e9c43e381f25cff9f1ea8
jun-k0 <wnsrlsladlek@naver.com>
2021-03-26 03:36:12 +0900

    관리자 추가

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+from .models import Upload
+from .models import Upload_file
+from .models import Like
+from .models import Comment
+
+admin.site.register(Profile)
+admin.site.register(Upload)
+admin.site.register(Upload_file)
+admin.site.register(Like)
+admin.site.register(Comment)
