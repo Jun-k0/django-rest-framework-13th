4d01e5079035faaf5ae3bb796ff2e24a15760f3e
jun-k0 <wnsrlsladlek@naver.com>
2021-03-26 01:07:29 +0900

    add user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    userid = models.CharField(max_length=10, verbose_name="아이디", unique=True)
+    password = models.CharField(max_length=10)
+    nickname = models.CharField(max_length=10, unique=True, default="")
+    user_image = models.ImageField(blank=True)
+    post_num = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.profile.username
+
