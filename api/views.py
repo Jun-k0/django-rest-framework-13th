55af63623daad6df944a2ec9c68c6ee791f1050d
jun-k0 <wnsrlsladlek@naver.com>
2021-04-01 21:49:06 +0900

    GET,POST method 추가

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from api.models import Upload
+from api.serializers import UploadSerializer
+
+
+@csrf_exempt
+def upload_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        upload = Upload.objects.all()
+        serializer = UploadSerializer(upload, many=True)
+        return JsonResponse(serializer.data,safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = UploadSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
