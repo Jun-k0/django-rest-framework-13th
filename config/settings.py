75f8dd74527eb3efa5a889aedb267e2def9fad9c
jun-k0 <wnsrlsladlek@naver.com>
2021-04-06 15:46:20 +0900

    TIME_ZONE 설정

--- config/settings.py
+++ config/settings.py
@@ -110,15 +110,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr' # 어드민 페이지 한국어
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul' # db 한국 시간
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False # True시 장고가 임의로 UTC 조정
 
 
 # Static files (CSS, JavaScript, Images)
