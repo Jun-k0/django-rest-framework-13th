2f4d9556c2461a42866cad013c4c09c9558a28eb
jun-k0 <wnsrlsladlek@naver.com>
2021-04-08 21:40:39 +0900

    view 추가

--- api/views.py
+++ api/views.py
@@ -3,15 +3,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from api.models import Upload
-from api.serializers import UploadSerializer
-
-
-@csrf_exempt
+from api.models import Profile, Upload
+from api.serializers import ProfileSerializer, UploadSerializer, UploadListSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+''' FBV 방식
+@csrf_exempt # 해킹 방지?
 def upload_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
     if request.method == 'GET':
         upload = Upload.objects.all()
         serializer = UploadSerializer(upload, many=True)
@@ -24,3 +23,46 @@ def upload_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+'''
+#image 파일 보완필요
+
+class UserList(APIView):
+    def get(self, request, format=None):
+        profile = Profile.objects.all()
+        serializer = ProfileSerializer(profile, many=True) # Queryset 일시, many=True
+        return JsonResponse(serializer.data, safe=False)
+    def post(self, request, format=None):
+        data = JSONParser().parse(request)
+        serializer = ProfileSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+class UserUpload(APIView):
+    def get(self, request, pk, format=None):
+        profile = Profile.objects.get(id=pk)
+        upload = Upload.objects.filter(profile=profile)
+        serializer = UploadListSerializer(upload, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    def post(self, request, pk, format=None):
+        data = JSONParser().parse(request)
+        serializer = UploadSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+class UploadFix(APIView):
+    def put(self, request, pk, format=None):
+        data = JSONParser().parse(request)
+        upload = Upload.objects.get(id=pk)
+        serializer = UploadSerializer(upload, data=data) # 수정 방식
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    def delete(self, request, pk, format=None):
+        upload = Upload.objects.get(id=pk)
+        upload.delete()
+        return Response(status=status.HTTP_200_OK)
