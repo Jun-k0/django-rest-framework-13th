998129e314e76df6948dd17b964e009c94098d89
jun-k0 <wnsrlsladlek@naver.com>
2021-04-08 23:27:12 +0900

    api/uploads/ 추가

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import UserList, UserUpload, UploadFix
+from .views import UserList, UserUpload, UploadFix, UploadList
 
 urlpatterns = [
     path('users/', UserList.as_view()),
     path('users/<int:pk>/uploads/', UserUpload.as_view()),
     path('uploads/<int:pk>/', UploadFix.as_view()),
-
+    path('uploads/', UploadList.as_view())
 ]
